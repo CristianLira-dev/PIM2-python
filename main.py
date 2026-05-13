475720c3b9f9e5da4f9f783e4f174d7b17b3ea48
Cristian Albertino de Lira Filho <cristianfilho2008@outlook.com>
2026-05-13 17:52:12 -0300

    Feat(classe Pet): Adicionando a Classe Pet e fazendo o salvamento no arquivo pets.json

--- main.py
+++ main.py
@@ -1,1 +1,44 @@
-#teste do video
+import json
+
+arquivo = "pets.json"
+dados = []
+
+class Pet:
+    def __init__(self, nome, idade, raca, porte, adotado=False):
+        self.nome = nome
+        self.idade = idade
+        self.raca = raca
+        self.porte = porte
+        self.adotado = adotado
+
+    def to_json(self):
+        return {
+            "nome": self.nome,
+            "idade": self.idade,
+            "raca": self.raca,
+            "porte": self.porte,
+            "adotado": self.adotado
+        }
+    
+pet1 = Pet(
+    "Thor",
+    "2 anos",
+    "Labrador",
+    "Grande"
+)
+pet2 = Pet(
+    "Batata",
+    "2 anos",
+    "Labrador",
+    "Grande",
+    True
+)
+
+
+dados.append(pet1.to_json())
+dados.append(pet2.to_json())
+
+with open(arquivo, "w", encoding="utf-8") as arq:
+    json.dump(dados, arq, indent=4, ensure_ascii=False)
+
+print("testar criação do arquivo")
