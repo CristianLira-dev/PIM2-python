a5a874b9211c8cd749f9c21ada3e5de8f1b4cfc4
Vinicius Pedon <viniciuspedon9@gmail.com>
2026-05-14 16:39:48 -0300

    criacao da classe pet

--- pet.py
+++ pet.py
@@ -1,2 +1,7 @@
-class pet:
-    def __init__(self, nome, ):
+class Pet:
+    def __init__(self, nome, idade, raca, porte, adotado=False):
+        self.nome = nome
+        self.idade = idade
+        self.raca = raca
+        self.porte = porte
+        self.adotado = adotado
