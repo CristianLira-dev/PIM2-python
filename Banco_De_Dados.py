0a2b7fffab2fdae8feaa5fb00384ac6e98d34da7
Vinicius Pedon <viniciuspedon9@gmail.com>
2026-05-14 18:28:32 -0300

    Criei um sistema de listagem a partir do banco de dados

--- Banco_De_Dados.py
+++ Banco_De_Dados.py
@@ -4,4 +4,8 @@ def armazenar_pets(pet):
     Pets.append(pet)
 
 def listar_pets():
-    print(Pets)
+    contador = 1
+    for pet in Pets:
+        print(f"{contador} - nome: {pet["nome"]} | idade: {pet["idade"]} | raca: {pet["raca"]} "
+              f"| porte: {pet["porte"]} | adotado: {pet["adotado"]}")
+        contador += 1
